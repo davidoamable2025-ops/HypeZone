98d201c49444b7467bb035a87149de250f3a6e60
davidoamable2025-ops <davidoamable2025@gmail.com>
2025-11-04 07:53:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function insta(){
+    window.open("https:instagram.com")
+}
+
+function tiktok(){
+    window.open("https:tiktok.com")
+}
+
+function anime(){
+    window.open("Https:anime-hive.vercel.app")
+}
+function chatgpt(){
+    window.open("https:chatgpt.com")
+}
+
+function sports(){
+    // window.open("sports.html")
+    window.location.href = "sports.html"
+}
+
+function game(){
+    window.location.href = "gaming.html"
+}
+
+
+function movie(){
+    window.open("https://moviebox.ph/web/movie")
+}
+// document.getElementById("sportstext" document.wite)
+
